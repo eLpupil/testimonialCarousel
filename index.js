84c3cba14f892490bc2fff0f893f63ec303dfec1
JX <jxie1995@gmail.com>
2020-05-19 01:27:17 -0700

    media query.

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ function nextSlide(event) {
 
 
     if (event.target.classList.contains("next")) {
-        $("#" + currentSlide).slideUp(200);
+        $("#" + currentSlide).fadeOut(300);
         currentSlide++;
         currentSlide = currentSlide % numSlides;
         console.log("next:" + currentSlide);
     }
     else if (event.target.classList.contains("prev")) {
-        $("#" + currentSlide).slideUp(200);
+        $("#" + currentSlide).fadeOut(300);
         currentSlide--;
         currentSlide = Math.abs(currentSlide) % numSlides;
         console.log("prev:" + currentSlide);
     }
 
-    $("#" + currentSlide).slideDown(200);
+    $("#" + currentSlide).fadeIn(300);
 }

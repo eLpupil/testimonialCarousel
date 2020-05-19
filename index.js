51878521ded894b5f7831d1c0769b85bc0d6918f
JX <jxie1995@gmail.com>
2020-05-18 23:32:31 -0700

    Carousel Animation completed

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-$(".btn-container .btn").on("click", function() {
-    console.log(event);
-})
+const numSlides = $(".carousel__track li").length;
+var currentSlide = numSlides - 1;
 
-function 
+$(".carousel__btn-container .carousel__btn").on("click", nextSlide);
+
+
+function nextSlide(event) {
+
+
+    if (event.target.classList.contains("next")) {
+        $("#" + currentSlide).slideUp(200);
+        currentSlide++;
+        currentSlide = currentSlide % numSlides;
+        console.log("next:" + currentSlide);
+    }
+    else if (event.target.classList.contains("prev")) {
+        $("#" + currentSlide).slideUp(200);
+        currentSlide--;
+        currentSlide = Math.abs(currentSlide) % numSlides;
+        console.log("prev:" + currentSlide);
+    }
+
+    $("#" + currentSlide).slideDown(200);
+}
